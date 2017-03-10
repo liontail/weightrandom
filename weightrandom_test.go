e4d13695a8286b6b0492239bfc58a03469988517
Phupatchayawit <phupatchayawit.s@mvisioncorp.com>
2017-03-10 12:35:18 +0700

    fix test file

--- weightrandom_test.go
+++ weightrandom_test.go
@@ -1,7 +1,6 @@
 package weightrandom
 
 import (
-	"log"
 	"testing"
 
 	. "github.com/franela/goblin"
@@ -10,26 +9,27 @@ import (
 
 func Test(t *testing.T) {
 	g := Goblin(t)
-	item1 := Items{
-		Weight: 10,
-		Data:   uint(1),
-	}
-	item2 := Items{
-		Weight: 10,
-		Data:   uint(2),
-	}
-	item3 := Items{
-		Weight: 80,
-		Data:   uint(3),
-	}
-	allItems := []Items{}
-	allItems = append(allItems, item1)
-	allItems = append(allItems, item2)
-	allItems = append(allItems, item3)
 
+	// Assign Item to All Items
+	allItems := []Items{
+		Items{
+			Weight: 10,
+			Data:   uint(1),
+		},
+		Items{
+			Weight: 10,
+			Data:   uint(2),
+		},
+		Items{
+			Weight: 80,
+			Data:   uint(3),
+		},
+	}
+
+	// Send All Items and No. of item that you want
 	result := DoRandom(allItems, 2)
-	log.Println(result)
 
+	// Expect Items with most weight
 	expect := false
 	for _, key := range *result {
 		if key.Data == uint(3) {
